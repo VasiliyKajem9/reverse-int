b677e126f651300606e6606844a7cd94cf4da102
VasiliyK <topsecret6261177aa@gmail.com>
2023-01-08 12:09:02 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
+  let numToStr = n + '';
+  if (n > 0) {
+    return +numToStr.split("").reverse().join("");
+  } else return +numToStr.slice(1).split("").reverse().join("");
   
 }
